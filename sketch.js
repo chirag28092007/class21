4df6db4fed4e2bc7abe4255576d2f23006fb766a
chirag28092007 <86223984+chirag28092007@users.noreply.github.com>
2021-12-10 04:19:45 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+if(isTouching(movingRect,fixedRect)){
+  movingRect.shapeColor = "red";
+  fixedRect.shapeColor = "red";
+}
+  else {
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+  
+  drawSprites();
+}
+function isTouching(o1,o2){
+  if (o1.x - o2.x < o2.width/2 + o1.width/2
+    && o2.x - o1.x < o2.width/2 + o1.width/2
+    && o1.y - o2.y < o2.height/2 + o1.height/2
+    && o2.y - o1.y < o2.height/2 + o1.height/2) {
+  return true 
+}
+else {
+return false
+    
+}
+}
